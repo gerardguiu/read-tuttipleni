d84e7300fde72a49d8148a14f8045b50cd94e17b
guiu <gerard@partfy.com>
2023-07-18 01:04:27 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# read-tuttipleni
+
+#read-tuttipleni
+
 Chome extension for read all blocked articles
+
+
+
+## Authors
+
+- [@gerardguiu](https://twitter.com/gerardguiu)
+
+
+## Installation
+
+**** Only for development ***
+
+- Download the files
+- Open chome > extensions
+- Click to the developer mode
+- Click to the 'upload files'
+
+
+## Usage
+
+Open the article and click to the extension and then a button "read article"
+
+
---
+++ readwebs.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("readUrl").addEventListener("click", function() {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            const googleCache = 'http://webcache.googleusercontent.com/search?q=cache:';
+            let url = tabs[0].url;
+           chrome.tabs.create({ url: googleCache+url });
+        // window.location.href = googleCache+url;
+        }); 
+    }, false);
+});
+
+
